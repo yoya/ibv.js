7c325167e92786f4ca25075e9b681b2ca836ca52
Yoshihiro Yamazaki <yoya@awm.jp>
2017-01-06 22:42:23 +0900

    wrong condition for lastOffset culculation.

--- container.js
+++ container.js
@@ -5,32 +5,51 @@ class ibvContainer {
 	// console.debug(container);
 	this.container = container;
     }
-    static toHexArray(arr) {
-	var hexArr = new Array(arr.length);
-	for (var i in arr) {
-	    var n = arr[i];
-	    var h = n.toString(16);
-	    if (n < 0x10) {
-		h = "0" + h;
-	    }
-	    hexArr[i] = h;
-	}
-	return hexArr;
-    }
     add(chunkList) {
 	var divContainer = document.createElement("div");
 	divContainer.className = "imgContainer";
 	for (var chunk of chunkList) {
-	    var div = document.createElement("div");
-	    div.className = "imgChunk";
+	    var divChunk = document.createElement("div");
+	    divChunk.className = "imgChunk";
 	    //
 	    var name = chunk.name;
 	    var bytes = chunk.bytes;
-	    var hexArray = this.constructor.toHexArray(bytes);
-	    var hexDump = "<tt>(<b>" + name + "</b>) " +hexArray.join(" ") + "</tt>";
-	    // console.log(hexDump);
-	    div.innerHTML = hexDump;
-	    divContainer.appendChild(div);
+	    var hexArray = null;
+	    var hexDump = "";
+	    if ("infos" in chunk) {
+		if (chunk.offset < chunk.infos[0].offset) {
+		    console.error("chunk.offset:"+ chunk.offset+"< chunk.infos[0].offset:"+chunk.infos[0].offset+" on "+name);
+		}
+		divChunk.innerHTML = "<b>(" + name + ")</b> ";
+		for (var idx in chunk.infos) {
+		    idx = idx >>> 0;
+		    var info = chunk.infos[idx];
+		    var offset = info.offset;
+		    if ((idx + 1) < chunk.infos.length) {
+			var nextOffset = chunk.infos[idx + 1].offset;
+		    } else {
+			var nextOffset = chunk.offset + bytes.length;
+		    }
+		    var divInfo = document.createElement("div");
+		    var divDump = document.createElement("div");
+		    divInfo.className = "imgInfo";
+		    divDump.className = "imgDump";
+		    console.log(name, nextOffset - offset, nextOffset, offset);
+		    var infoBytes = bytes.subarray(0, nextOffset - offset);
+		    var infoJson = JSON.stringify(info, null, " ");
+		    hexArray = toHexArray(infoBytes);
+		    hexDump = hexArray.join(" ");
+		    divInfo.innerHTML = "<tt>" + infoJson + "</tt>";
+		    divDump.innerHTML = "<tt>" + hexDump + "</tt>";
+		    divChunk.appendChild(divInfo);
+		    divChunk.appendChild(divDump);
+		}
+	    } else {
+		hexArray = toHexArray(bytes);
+		hexDump = hexArray.join(" ");
+		divChunk.innerHTML = "<b>(" + name + ")</b> <tt> " + hexDump + "</tt>";
+	    }
+	    divContainer.appendChild(divChunk);
 	}
 	this.container.appendChild(divContainer);
     }
