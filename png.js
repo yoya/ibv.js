d3e07f2c7be9b3ffab3474cc6d0cc36c0263ebe8
Yoshihiro Yamazaki <yoya@awm.jp>
2017-01-07 03:15:48 +0900

    add png signature to infos. fix crc32 info

--- png.js
+++ png.js
@@ -19,25 +19,27 @@ class IO_PNG {
     }
     parse(arr) {
 	this.data = arr;
-	var chunkArray = [];
+	var chunk = {name:"Signature", offset:0, bytes:this.constructor.signature()};
+	var chunkArray = [chunk];
 	var arrLen = arr.length;
 	var bo = 8; //bo: byteOffset(& baseOffset);
 	var bytes;
 	while (bo < arrLen) {
 	    var len = ((arr[bo]*0x100 + arr[bo+1])*0x100 + arr[bo+2])*0x100 + arr[bo+3];
 	    var name = Utils.toText(arr.subarray(bo + 4, bo + 8));
-	    var o = bo + 8 + len;
 	    var chunk = {name:name, offset:bo, bytes:null, crc32:null, infos:null};
 	    var infos = [{offset:bo, len:len}];
 	    infos.push({offset:bo+4, name:name});
-	    bytes = arr.subarray(bo, o);
-	    chunk.bytes = bytes;
-	    chunk.infos = infos;
-	    infos.push({offset:bo+8, nBytes:bytes.length - 8});
+	    infos.push({offset:bo+8, nBytes:len});
+	    var o = bo + 8 + len;
 	    var crc32  = ((arr[o]*0x100 + arr[o+1])*0x100 + arr[o+2])*0x100 + arr[o+3];
 	    chunk.crc32 = crc32;
-	    infos.push({offset:bo+8+len, crc32:crc32});
-	    bo = o + 4;
+	    infos.push({offset:o, crc32:crc32});
+	    o += 4;
+	    bytes = arr.subarray(bo, o);
+	    bo = o;
+	    chunk.bytes = bytes;
+	    chunk.infos = infos;
 	    chunkArray.push(chunk);
 	    if (name  === "IEND") {
 		break;
