28ac6465f268d9c39c45ea7877c3e9af687f20e9
Yoshihiro Yamazaki <yoya@awm.jp>
2017-01-10 20:55:46 +0900

    ToHexArray refine.

--- utils.js
+++ utils.js
@@ -3,18 +3,18 @@
 var Utils = {};
 
 Utils.ToText = function (arr) {
-    return String.fromCharCode.apply("", arr);
+    return String.fromCharCode.apply(null, arr);
+};
+
+Utils.ToHex = function(n) {
+    var h = n.toString(16);
+    return (n < 0x10)? ("0"+h) : h;
 };
 
 Utils.ToHexArray = function(arr) {
     var hexArr = new Array(arr.length);
     for (var i in arr) {
-	var n = arr[i];
-	var h = n.toString(16);
-	if (n < 0x10) {
-	    h = "0" + h;
-	}
-	hexArr[i] = h;
+	hexArr[i] =  Utils.ToHex(arr[i]);
     }
     return hexArr;
 };
