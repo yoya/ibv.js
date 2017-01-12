930ffe4699a5d59abea31d1d31aed6171b4a0c80
Yoshihiro Yamazaki <yoya@awm.jp>
2017-01-12 20:22:32 +0900

    add IO_TIFF

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function main() {
     var containerNode = document.getElementById("ibv_container");
-    var ibviewer = new ImageBinaryViewer(containerNode, [IO_JPEG, IO_PNG, IO_GIF]);
+    var ibviewer = new ImageBinaryViewer(containerNode, [IO_JPEG, IO_PNG, IO_GIF, IO_TIFF]);
     dropFunction(null, function(buf) {
 	if (buf === null) {
 	    ibviewer.reset();
